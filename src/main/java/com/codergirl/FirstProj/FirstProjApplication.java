a4e6cc9769fe2829aab5d7b4f0a6680bf7bdd3f0
Lauren Workman <lauren.e.workman@gmail.com>
2019-07-17 20:22:50 -0400

    First project commit

---
+++ src/main/java/com/codergirl/FirstProj/FirstProjApplication.java
@@ -0,0 +1,13 @@
+package com.codergirl.FirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+	}
+
+}
